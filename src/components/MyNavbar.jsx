f1a6a7485f71d4064507465f99d4654ab46d98e8
Batı Gökovalı <batigokovali@icloud.com>
2023-02-03 18:07:35 +0300

    update

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,8 +1,10 @@
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Component } from "react";
-import FaSearch from 'react-icons/fa';
+import { Search } from 'react-bootstrap-icons';
+import { Bell } from 'react-bootstrap-icons';
+import Dropdown from 'react-bootstrap/Dropdown';
+
 
 
 class MyNavbar extends Component {
@@ -15,11 +17,23 @@ class MyNavbar extends Component {
                 <Navbar.Collapse id="basic-navbar-nav">
                   <Nav className="me-auto">
                     <Nav.Link href="#home" className="text-white">Home</Nav.Link>
-                    <FaSearch></FaSearch>
+                    
                   </Nav>
                 </Navbar.Collapse>
-                <div className='ml-auto'>
-                      <img src="" alt="" />
+                <div className='ml-auto d-flex justify-content-center align-items-center'>
+                <Search className='text-white'></Search>
+                <p className='text-white mb-0 mx-3'>KIDS</p>
+                <Bell className='text-white'></Bell>
+                <Dropdown align="end" bg="transparent">
+      <Dropdown.Toggle id="dropdown" bg="transparent" align="end">
+        <img className="avatar-img mr-2" src="https://media.npr.org/assets/img/2020/10/06/gettyimages-74299482_wide-5ac106c73086aa63c495089c35f4811d0bb1ce5b.jpg" alt="" />
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu align="end" bg="transparent">
+        <Dropdown.Item href="#/action-1">Edit Profile</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Account</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
                     </div>
               
             </Navbar>
